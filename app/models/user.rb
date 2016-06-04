8977012ad8efff5100e2ae736515e77c0888c8c2
xavilens <xavilens@gmail.com>
2016-06-04 13:44:10 +0200

    Modificado modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :userable_id, presence: true
+  # Con validates campoX uniqueness: {scope: campoY} creamos unicidad en la tupla de campos {campoX, campoY},
+  # imposibilitando crear un Usuario con valores repetidos en dicha tupla
+  validates :userable_type, presence: true, uniqueness: {scope: :userable_id}
+
+  belongs_to :userable, polymorphic: true
 end
