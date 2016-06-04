b6e30c0f1d32b81b5278081f3742101099fc4fd7
xavilens <xavilens@gmail.com>
2016-06-04 13:41:27 +0200

    Devise instalado

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Define url por defecto para Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
