bcaf769cbd4c6faf3e5a584965b2526f0c11c44c
xavilens <xavilens@gmail.com>
2016-06-05 15:33:40 +0200

    Añadidas persistencias a modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,22 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # CAMPOS: id, email, encrypted_password, nombre, ciudad, comunidad, pais,
+  # userable_id, userable_type, rol_id, created_at, updated_at
+
+  #CAMPOS DEVISE: reset_password_token, reset_password_sent_at,
+  # remember_created_at, sign_in_count, current_sign_in_at, last_sign_in_at,
+  # current_sign_in_ip, last_sign_in_ip
+
+  validates :ciudad, presence: true
+  validates :comunidad, presence: true
+  validates :pais, presence: true
+  validates :rol_id, presence: true, foreign_key: :rol
   validates :userable_id, presence: true
   # Con validates campoX uniqueness: {scope: campoY} creamos unicidad en la tupla de campos {campoX, campoY},
   # imposibilitando crear un Usuario con valores repetidos en dicha tupla
   validates :userable_type, presence: true, uniqueness: {scope: :userable_id}
 
-  has_one :role
+  has_one :rol
   belongs_to :userable, polymorphic: true
 end
