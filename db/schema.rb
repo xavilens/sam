bf884f960ba355a22a8eeb1babbb5f0ed761b49b
xavilens <xavilens@gmail.com>
2016-06-09 23:38:18 +0200

    Puesta a punto

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604114248) do
+ActiveRecord::Schema.define(version: 20160606204126) do
+
+  create_table "rols", force: :cascade do |t|
+    t.string   "descripcion", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
