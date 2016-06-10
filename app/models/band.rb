9c0a97c9b12957a242267e7b3a8d2d45152a028f
xavilens <xavilens@gmail.com>
2016-06-10 20:24:14 +0200

    Migrations done

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,5 +1,8 @@
 class Band < ActiveRecord::Base
-  belongs_to :genre1, class_name: :genre
-  belongs_to :genre2, class_name: :genre
-  belongs_to :genre3, class_name: :genre
+  has_one :user, as: :profileable
+  belongs_to :genre1, class_name: 'Genre'
+  belongs_to :genre2, class_name: 'Genre'
+  belongs_to :genre3, class_name: 'Genre'
+
+  # TODO: Campo Estado = {buscando miembros, buscando conciertos, ninguno}
 end
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,8 +1,9 @@
 class Musician < ActiveRecord::Base
+  has_one :user, as: :profileable
   has_many :musician_knowledges
   has_many :knowledges, through: :musician_knowledges
 
-  # TODO: Campo Estado = {}
+  # TODO: Campo Estado = {buscando grupo, ninguno}
 
   def knowledges
     self.musician_knowledges
