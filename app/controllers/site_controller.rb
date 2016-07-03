9e5e2928ad10337f7f2ab8c6f6a5c2ae921cc0ee
xavilens <xavilens@gmail.com>
2016-07-03 20:52:13 +0200

    Added admin_id attribute to User class

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,11 +1,8 @@
 class SiteController < ApplicationController
-  
+
   def index
     @hola = "Hola, mundo!"
 
-    if User.admin_id.nil?
-      User.initialize_admin_id
-    end
-
+    User.initialize_admin_id
   end
 end
