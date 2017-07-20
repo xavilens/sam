9d49a9bcdec1b126c302e176559b9b5e7993d9ca
xavilens <xavilens@gmail.com>
2017-07-20 14:02:33 +0200

    Site landing fixed

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -2,7 +2,7 @@ module LandingHelper
   # Devuelve al azar una imagen de fondo
   # ref: http://stackoverflow.com/questions/14404801/random-background-picture-css-rails
   def randomized_background_image
-    images = ["back-7.jpg", "back-1.jpg", "back-2.jpg", "back-3.jpg", "back-4.jpg", "back-5.jpg", "back-6.jpg"]
+    images = ["back-1.jpg", "back-2.jpg", "back-3.jpg", "back-4.jpg", "back-5.jpg", "back-6.jpg", "back-7.jpg"]
     image = images[rand(images.size)]
 
     return asset_path "landing/#{image}"
